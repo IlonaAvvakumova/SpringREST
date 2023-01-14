f5cfe848386e8f3a65f2b2769647b14dee1810d9
Ilona <avvakumova_ilona@mail.ru>
2023-01-14 13:51:39 +0300

    1

--- src/main/java/com/filesrest/config/SecurityConfig.java
+++ src/main/java/com/filesrest/config/SecurityConfig.java
@@ -1,16 +1,14 @@
 package com.filesrest.config;
-import com.filesrest.service.JwtConfigurer;
+import com.filesrest.service.JwtConfigure;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -18,9 +16,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final JwtConfigurer jwtConfigurer;
+    private final JwtConfigure jwtConfigurer;
 
-    public SecurityConfig(JwtConfigurer jwtConfigurer) {
+    public SecurityConfig(JwtConfigure jwtConfigurer) {
         this.jwtConfigurer = jwtConfigurer;
     }
 
